7459393321eb93438d2fee300e423a8283d1591f
Luciana Ortiz <luciana.ortiz@alumnos.frm.utn.edu.ar>
2024-10-21 23:14:35 -0300

    video 7

---
+++ src/persistence/localstorage.js
@@ -0,0 +1,18 @@
+export const getProductLocalStorage  = () => {
+    const products = JSON.parse(localStorage.getItem('products'));
+    return products ?  products : [];
+}
+
+export const setProductLocalStorage = (product) => {
+    let productsInLocalStorage =  getProductLocalStorage();
+
+    const existingIndex  = productsInLocalStorage.findIndex(item => item.id === product.id);
+
+    if (existingIndex !== -1) {
+        productsInLocalStorage[existingIndex]  = product;
+    } else {
+        productsInLocalStorage.push(product);
+    }
+
+    localStorage.setItem('products', JSON.parse(productsInLocalStorage))
+}
